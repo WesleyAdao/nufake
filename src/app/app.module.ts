e43650073e79b8a786ad05eb87780a7fe482acfb
Wesley Adao <wesleyadao88@hotmail.com>
2021-02-23 15:29:14 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NufakeHomeComponent } from './nufake-home/nufake-home.component';
-import { NufakeLoginComponent } from './nufake-login/nufake-login.component';
-import { NufakeRecoverypassComponent } from './nufake-recoverypass/nufake-recoverypass.component';
-import { NufakeErrorComponent } from './nufake-error/nufake-error.component';
-import { NufakeDashComponent } from './nufake-dash/nufake-dash.component';
+import { NufakeHomeComponent } from './shared/components/nufake-home/nufake-home.component';
+import { NufakeLoginComponent } from './shared/components/nufake-login/nufake-login.component';
+import { NufakeRecoverypassComponent } from './shared/components/nufake-recoverypass/nufake-recoverypass.component';
+import { NufakeErrorComponent } from './shared/components/nufake-error/nufake-error.component';
+import { NufakeDashComponent } from './shared/components/nufake-dash/nufake-dash.component';
 
 @NgModule({
   declarations: [
