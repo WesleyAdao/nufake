18b6346b4f389742c29b5b62063b90dfd73458d9
maksoncavalcanti <maksoncvalcanti@gmail.com>
2021-02-25 23:51:29 -0300

    referencias de default-card

--- src/app/shared/components/default-card/default-card.component.ts
+++ src/app/shared/components/default-card/default-card.component.ts
@@ -7,13 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultCardComponent implements OnInit {
 
-  iconeCard = 'default-header-image';
-  titulo = '';
-  subtitulo = '';
+  iconeCardConta = 'default-header-conta';
+  titulo = 'Conta';
+  subtitulo = 'Saldo Disponivel';
   labelValor = 'Transações:';
   valor1 = '10.000,00';
   valor2 = '2.120,21';
 
+  iconeCardCredito = 'default-header-credito';
+  tituloCredito = 'Conta Credito';
+  subtituloCredito = 'Fatura atual';
+  labelValorCredito = 'Limite disponivel';
+  valor3 = '120,88';
+  valor4 = '9.120,88';
+
+  iconeCardLancamentos = 'default-header-lancamento';
+  tituloLancamentos = 'Ultimos lançamentos';
+  subtituloLancamentos = 'Compra no débito';
+  labelValorLancamentos = 'GamaAcademy';
+  valor5 = '120,88';
 
   constructor() { }
 
