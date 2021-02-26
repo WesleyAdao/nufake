21a8f059eef19155dad914dbc9b1e4a6d7e5f1d5
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-26 12:43:13 -0300

    criado chamada na API para reset de senha

--- src/app/shared/components/nufake-recoverypass/nufake-recoverypass.component.ts
+++ src/app/shared/components/nufake-recoverypass/nufake-recoverypass.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
+import { NufakeRecoverypassService } from './nufake-recoverypass.service';
 
 @Component({
   selector: 'app-nufake-recoverypass',
@@ -13,9 +15,8 @@ export class NufakeRecoverypassComponent implements OnInit {
 
   cpfValido: any;
 
-
-
   constructor(
+    private nufakeRecoverypassService: NufakeRecoverypassService,
     private formBuilder: FormBuilder,
     private router: Router
   ) { }
@@ -32,11 +33,11 @@ export class NufakeRecoverypassComponent implements OnInit {
     })
   }
 
-
-
   //MOCK atraves do loginService pelo objeto {data}
   enviarForm() {
-    const usuario = localStorage.getItem('data');
+  // this.callAPI()
+
+    const usuario = localStorage.getItem('cpf');
     let login;
 
     if (usuario) {
@@ -44,14 +45,22 @@ export class NufakeRecoverypassComponent implements OnInit {
     }
 
     const form = this.formRecovery.getRawValue();
-      if (form.cpf === login.cpf && form.senha === form.senhaRepete) {
+    if (form.cpf === login.cpf && form.senha === form.senhaRepete) {
       this.router.navigate(['dashboard'])
 
     } else {
       alert('Cpf ou senha incorretos');
     }
+  }
 
-
+  callAPI() {
+    this.nufakeRecoverypassService.solicitarNovaSenha()
+    .pipe(
+      take(1)
+    ).subscribe(
+      response => this.nufakeRecoverypassService.alterarSenha(response),
+      error => this.nufakeRecoverypassService.alterarSenha(error.error.text)
+    )
   }
 
 }
