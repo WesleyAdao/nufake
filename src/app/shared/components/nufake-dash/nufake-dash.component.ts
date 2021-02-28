fa806e3f0097ef1707734f12bc2121a9a214793c
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-27 22:41:36 -0300

    Criado todas as chamadas a API para o dashboard

--- src/app/shared/components/nufake-dash/nufake-dash.component.ts
+++ src/app/shared/components/nufake-dash/nufake-dash.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { pipe } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { Dashboard } from '../../interfaces/Dashboard.interface';
-
+import { PlanoConta } from '../../interfaces/lancamento/planos-conta/PlanoConta.interface';
 import { CardListComponent } from '../card-list/card-list.component';
 import { DefaultCardComponent } from '../default-card/default-card.component';
 import { NufakeDashService } from './nufake-dash.service';
+
 
 @Component({
   selector: 'app-nufake-dash',
@@ -24,10 +27,72 @@ export class NufakeDashComponent implements OnInit {
   ngOnInit(): void {
 
     this.nufakeDash.dashboard()
-      .subscribe(
+      .pipe(
+        take(1)
+      ).subscribe(
         response => this.dashboard = response
       )
+
+    // Chamando a API para getPlanosConta
+    //this.listaPlanos()
+
+    // Chamando a API para criação de plano
+    //valores pegos dos campos
+    //this.criaPlanos("Salario do mês", "mandre", "D")
+
+    // Chamando a API de lançamentos
+    //this.criaLancamento()
   }
 
+  // listaPlanos() {
+  //   planoConta: PlanoConta[]
+  //   this.nufakeDash.getPlanosConta()
+  //     .pipe(
+  //       take(1)
+  //     ).subscribe(
+  //       response => this.planoConta = response
+  //     )
+  // }
+
+  // criaPlanos(descricao: string, login: string, tipoMovimento: string) {
+  //   this.nufakeDash.planosConta({ descricao, login, tipoMovimento })
+  //     .pipe(
+  //       take(1)
+  //     ).subscribe(
+  //       response => this.onCreatePlansSuccess()
+  //       error => this.onErrorOnDashboard()
+  //     )
+  // }
+
+  // criaLancamento() {
+
+  //   Esses valores são pegos dos campos dos lançamento
+  //   this.nufakeDash.lancamentos({
+  //     conta: 827,
+  //     data: "2021-02-27",
+  //     descricao: "PAGAMENTO CONTA DE INTERNET",
+  //     login: "mandre",
+  //     planoConta: 1610,
+  //     valor: 1500
+  //   })
+  //     .pipe(
+  //       take(1)
+  //     ).subscribe(
+  //       response => this.onCreateLancamentos(),
+  //       error => this.onErrorOnDashboard()
+  //     )
+  // }
+
+  // onCreatePlansSuccess() {
+  //   console.log("Criação de plano bem sucedida")
+  // }
+
+  // onCreateLancamentos() {
+  //   console.log("Lançamento realizado com sucesso")
+  // }
+
+  // onErrorOnDashboard() {
+  //   console.log("OOps!, algo de errado aconteceu")
+  // }
 
 }
--- src/app/shared/components/nufake-dash/nufake-dash.service.ts
+++ src/app/shared/components/nufake-dash/nufake-dash.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 import { Dashboard } from '../../interfaces/Dashboard.interface';
+import { LancamentoDTO } from '../../interfaces/lancamento/LancamentoDTO.interface';
+import { PlanoConta } from '../../interfaces/lancamento/planos-conta/PlanoConta.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,24 +12,42 @@ import { Dashboard } from '../../interfaces/Dashboard.interface';
 export class NufakeDashService {
   API_URL = environment.API_URL
 
-  httpParams = new HttpParams({
-    fromObject: {
-      fim: this.getData(),
-      inicio: this.getData(),
-      login: "mandre"
-    }
-  })
-
   constructor(
     private http: HttpClient
   ) { }
 
   dashboard(): Observable<Dashboard> {
-    return this.http.get<Dashboard>(`${this.API_URL}/dashboard`, { params: this.httpParams })
+    const httpParams = new HttpParams({
+      fromObject: {
+        fim: this.getData(),
+        inicio: this.getData(),
+        login: "mandre"
+      }
+    })
+    return this.http.get<Dashboard>(`${this.API_URL}/dashboard`, { params: httpParams })
   }
 
   getData(): string {
     return new Date().getFullYear() + "-" + new Date().toLocaleString("pt-BR", { month: "2-digit" }) + "-" + new Date().toLocaleString("pt-BR", { day: "2-digit" })
   }
 
+  lancamentos(lancamento: LancamentoDTO): Observable<void> {
+    return this.http.post<void>(`${this.API_URL}/lancamentos`, lancamento)
+  }
+
+  getPlanosConta(): Observable<PlanoConta[]> {
+    const httpParams = new HttpParams({
+      fromObject: {
+        login: "mandre"
+      }
+    })
+
+    return this.http.get<PlanoConta[]>(`${this.API_URL}/lancamentos/planos-conta`, { params: httpParams })
+  }
+
+  planosConta(conta: Omit<PlanoConta, 'id'|'padrao'>) {
+    const { descricao, login, tipoMovimento } = conta
+
+    return this.http.post(`${this.API_URL}/lancamentos/planos-conta`, { descricao, login, tipoMovimento })
+  }
 }
