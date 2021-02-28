688cefe5b24f1cb23f2f60325424a00ae55b0500
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-02-28 20:52:43 -0300

    Recebendo arquivos restantes do dashboard

--- src/app/shared/components/nufake-dash/nufake-dash.component.ts
+++ src/app/shared/components/nufake-dash/nufake-dash.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { NgForm } from '@angular/forms';
-import { pipe } from 'rxjs';
-=======
->>>>>>> 1654b8950b5ca34367d1474c70bc15ad97636bc6
 import { take } from 'rxjs/operators';
 import { Dashboard } from '../../interfaces/Dashboard.interface';
 import { PlanoConta } from '../../interfaces/lancamento/planos-conta/PlanoConta.interface';
@@ -24,13 +19,10 @@ export class NufakeDashComponent implements OnInit {
   cardList: CardListComponent;
   defaultCard: DefaultCardComponent;
 
-<<<<<<< HEAD
-=======
   planoConta: PlanoConta[];
 
   dashboard: Dashboard;
 
->>>>>>> 1654b8950b5ca34367d1474c70bc15ad97636bc6
   constructor(
     private nufakeDash: NufakeDashService
   ) { }
@@ -42,71 +34,13 @@ export class NufakeDashComponent implements OnInit {
       .pipe(
         take(1)
       ).subscribe(
-        response => { this.dashboard = response; 
+        response => { this.dashboard = response;
         console.log(response);
         }
       );
 
-      
 
-<<<<<<< HEAD
-  }
 
-  //  **** LISTA PLANOS ****
-  // listaPlanos() {
-  //   planoConta: PlanoConta[]
-  //   this.nufakeDash.getPlanosConta()
-  //     .pipe(
-  //       take(1)
-  //     ).subscribe(
-  //       response => this.planoConta = response
-  //     )
-  // }
-
-  //  **** CRIA UM DEPOSITO ****
-  //   Esses valores são pegos dos campos de depósito
-  // criaPlanos(descricao: string, login: string, tipoMovimento: string) {
-  //   this.nufakeDash.planosConta({ descricao, login, tipoMovimento })
-  //     .pipe(
-  //       take(1)
-  //     ).subscribe(
-  //       response => this.onCreatePlansSuccess()
-  //       error => this.onErrorOnDashboard()
-  //     )
-  // }
-
-  //  **** FAZ TRANSFERÊNCIAS ****
-  // criaLancamento() {
-
-  //   Esses valores são pegos dos campos da transferência
-  //   this.nufakeDash.lancamentos({
-  //     conta: 827,
-  //     data: "2021-02-27",
-  //     descricao: "PAGAMENTO CONTA DE INTERNET",
-  //     login: "mandre",
-  //     planoConta: 1610,
-  //     valor: 1500
-  //   })
-  //     .pipe(
-  //       take(1)
-  //     ).subscribe(
-  //       response => this.onCreateLancamentos(),
-  //       error => this.onErrorOnDashboard()
-  //     )
-  // }
-
-  // onCreatePlansSuccess() {
-  //   console.log("Criação de plano bem sucedida")
-  // }
-
-  // onCreateLancamentos() {
-  //   console.log("Lançamento realizado com sucesso")
-  // }
-
-  // onErrorOnDashboard() {
-  //   console.log("OOps!, algo de errado aconteceu")
-  // }
-=======
     // Chamando a API para getPlanosConta
     this.listaPlanos()
 
@@ -168,5 +102,4 @@ export class NufakeDashComponent implements OnInit {
     console.log("OOps!, algo de errado aconteceu")
   }
 
->>>>>>> 1654b8950b5ca34367d1474c70bc15ad97636bc6
 }
